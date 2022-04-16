9e7cd511ca5891f7fdf27f99bed55a1cd28f69a3
lucaviness <66859401+lucaviness@users.noreply.github.com>
2022-04-16 00:23:31 -0700

    Edited Tutorial Transformation
    Changed the transformation time from 0.33s to 1s for a smoother and cleaner look.

--- script.js
+++ script.js
@@ -1428,11 +1428,11 @@ function addClicks(){
   document.getElementById("menu_icon").onclick = function(){
     toggleElement(document.getElementById("tutorial_container"))
     window.setTimeout(function(){
-    document.getElementById("tutorial_box_1_0").style.transition = "transform 0.33s ease-in-out"
+    document.getElementById("tutorial_box_1_0").style.transition = "transform 1s ease-in-out"
     document.getElementById("tutorial_box_1_0").style.transform = "rotate3d(1,0,0,360deg)"
-    document.getElementById("tutorial_box_2_1").style.transition = "transform 0.33s ease-in-out"
+    document.getElementById("tutorial_box_2_1").style.transition = "transform 1s ease-in-out"
     document.getElementById("tutorial_box_2_1").style.transform = "rotate3d(1,0,0,360deg)"
-    document.getElementById("tutorial_box_3_3").style.transition = "transform 0.33s ease-in-out"
+    document.getElementById("tutorial_box_3_3").style.transition = "transform 1s ease-in-out"
     document.getElementById("tutorial_box_3_3").style.transform = "rotate3d(1,0,0,360deg)"
   },400)
   }
