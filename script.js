f43b5c2801fe403fe9e140f9266df25fec17a6c9
lucaviness <66859401+lucaviness@users.noreply.github.com>
2022-04-29 18:36:25 -0700

    Begin Refactoring

--- script.js
+++ script.js
@@ -330,150 +330,169 @@ function keyDown(event) {
 //Big function, anything typing and box related pretty much
 
 function renderLetter(key) {
-  if (!finished && !isEndScreenOpen && !animatingBox && !menuOpen) {
-    let box = document.getElementById(target);
-    let qwerty = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-    if (qwerty.includes(key) == true) {
-      if (box.textContent == ""){
-        box.textContent = key;
-        box.style.backgroundColor = "var(--white)";
-        box.style.transition = ""
-        box.style.borderColor = "var(--filledborder)";
-        box.style.color = "var(--black)";
-        let new_id = parseInt(target.substring(13))
-        new_id += 1
-        if (new_id>=box.parentElement.children.length){
-          new_id = box.parentElement.children.length-1
-        }
-        target = "letter_box_"+target_row+"_"+new_id
-      }
-    } else if (key == "DELETE" || key == "Backspace") {
-      let new_id = parseInt(target.substring(13))
-      let qwerty = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
-      if (new_id != 3){
-        new_id -= 1
-        if (new_id<0){
-          new_id = 0
-        }
-        target = "letter_box_"+target_row+"_"+new_id
-      }else if (new_id == 3 && qwerty.includes(box.textContent) == false){
-        new_id -= 1
-        target = "letter_box_"+target_row+"_"+new_id
-        }
-      box = document.getElementById(target);
-      box.textContent = "";
-      box.style.backgroundColor = "var(--white)";
-      box.style.borderColor = "var(--defaultborder)";
-      box.style.color = "var(--black)";
-    } else if (key == "ENTER" || key == "Enter") {
-      if (box == box.parentElement.lastChild && box.textContent != ""){ 
-        let guess = ""
-        for (let i = 0; i<box.parentElement.children.length;i++){ 
-          guess += box.parentElement.children[i].textContent
-        }
-        if (airportArray.includes(guess) && guessedCodes.includes(guess) != true){
-          let fluidAnswer = [];
-          guessedCodes.push(guess)
-          for (i=0; i < answer.length; i++) {
-            fluidAnswer.push(answer[i]); 
-          }
-          correctCount = 0;
-          for (let i = 0; i<box.parentElement.children.length;i++) {
-            box.parentElement.children[i].style.transition = "transform " + (i*durationModif+1) + "s ease-in-out, background-color 0.2s " + (i*durationModif+0.8) + "s, border-color 0.2s " + (i*durationModif+0.8) + "s, color 0.2s " + (i*durationModif+0.8) + "s"
-            box.parentElement.children[i].style.transform = "rotate3d(1,0,0,"+ (Math.round((i+3)*360))+"deg)"
-            if (!animatingBox){
-              animatingBox = true
-              window.setTimeout(function(){animatingBox = false},(3000*durationModif+1000))
-            }
-            let guess = box.parentElement.children[i].textContent;
-            let transition = "background-color 0.2s " + (3*durationModif+1.1) + "s, border-color 0.2s "+(3*durationModif+1.1)+"s, color 0.2s "+(3*durationModif+1.1)+"s"
-            if (guess == fluidAnswer[i]) {
-              box.parentElement.children[i].style.backgroundColor = "var(--green)";
-              box.parentElement.children[i].style.borderColor = "var(--green)";
-              box.parentElement.children[i].style.color = "white";
-              document.getElementById(guess).style.transition = transition
-              document.getElementById(guess).style.backgroundColor = "var(--green)"
-              document.getElementById(guess).style.borderColor = "var(--green)"
-              document.getElementById(guess).style.color = "white"
-              fluidAnswer[i] = "";
-              correctCount += 1;
-            } else if (guess != fluidAnswer[i] && fluidAnswer.includes(guess) == true) {
-              let location = fluidAnswer.indexOf(guess); 
-              if (box.parentElement.children[location].textContent != fluidAnswer[location]){
-                box.parentElement.children[i].style.backgroundColor = "var(--yellow)"; 
-                box.parentElement.children[i].style.borderColor = "var(--yellow)";
-                box.parentElement.children[i].style.color = "white";
-                if (document.getElementById(guess).style.backgroundColor != "var(--green)"){
-                  document.getElementById(guess).style.transition = transition
-                  document.getElementById(guess).style.backgroundColor = "var(--yellow)"
-                  document.getElementById(guess).style.borderColor = "var(--yellow)"
-                  document.getElementById(guess).style.color = "white"
-                }
-                fluidAnswer[location] = "";
-              } else {
-                box.parentElement.children[i].style.backgroundColor = "var(--darkgrey)";
-                box.parentElement.children[i].style.borderColor = "var(--darkgrey)";
-                box.parentElement.children[i].style.color = "white";
-              }
-            } else if (fluidAnswer.includes(guess) == false) {
-              box.parentElement.children[i].style.backgroundColor = "var(--darkgrey)";
-              box.parentElement.children[i].style.borderColor = "var(--darkgrey)";
-              box.parentElement.children[i].style.color = "white";
-              let key = document.getElementById(guess)
-              if (key.style.backgroundColor != "var(--green)" || key.style.backgroundColor == "var(--yellow)"){
-                document.getElementById(guess).style.transition = transition
-                key.style.backgroundColor = "var(--darkgrey)"
-                key.style.borderColor = "var(--darkgrey)"
-                document.getElementById(guess).style.color = "white"
-              }
-            }
-          }
-          target_row += 1
-          if (target_row == box.parentElement.parentElement.children.length || correctCount == 4) { 
-            finished = true;
-          }
-          if (target_row == 6 && correctCount != 4) {
-            correct = false;
-          } else if (correctCount == 4) {
-            correct = true;
-            window.localStorage.setItem("updateGraph", "true")
-          }
-          if (target_row < box.parentElement.parentElement.children.length && correct == false){
-            window.setTimeout(moveDel, (3000*durationModif+1000))
-          }
-          target = "letter_box_"+target_row+"_"+0
-          if (finished == true) { 
-            updateStats(target_row,correct)
-            timer = window.setTimeout(endScreen, 3000*durationModif+1100)
-          }
-        } else {
-          if (typeof(document.getElementById("not_a_code_msg")) == 'undefined' || document.getElementById("not_a_code_msg") == null){
-            let error = document.createElement("div")
-            error.id = "not_a_code_msg"
-            if (guessedCodes.includes(guess)){
-              error.textContent = "Code Already Guessed"
-            }else {
-              if (airportArray == intlCodesArray && allCodesArray.includes(guess)){
-                error.textContent = "Only Intl. Codes Allowed. See settings to change."
-              }else {
-                error.textContent = "Invalid/Missing ICAO Code"
-              }
-            }
-            error.style.top = (document.getElementById("main_container").offsetTop *1.25) + "px";
-            document.body.append(error)
-            error.style.left = (((document.body.clientWidth + 15) / 2) - (error.clientWidth / 2)) + "px"; 
-            error.style.transition = "opacity 1s 1s"
-            error.style.opacity = 0
-            window.setTimeout(function(){
-              error.remove()
-            },2000)
-          }
-        }
-      }
-    }
-  }
+	if (!finished && !isEndScreenOpen && !animatingBox && !menuOpen) {
+		let box = document.getElementById(target);
+		let qwerty = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"];
+		if (qwerty.includes(key) == true) {
+			if (box.textContent == ""){
+			box.textContent = key;
+			box.style.backgroundColor = "var(--white)";
+			box.style.transition = ""
+			box.style.borderColor = "var(--filledborder)";
+			box.style.color = "var(--black)";
+			let new_id = parseInt(target.substring(13))
+			new_id += 1
+			if (new_id>=box.parentElement.children.length){
+				new_id = box.parentElement.children.length-1
+			}
+			target = "letter_box_"+target_row+"_"+new_id
+			}
+		} else if (key == "DELETE" || key == "Backspace") {
+			let new_id = parseInt(target.substring(13))
+			if (new_id != 3){
+			new_id -= 1
+			if (new_id<0){
+				new_id = 0
+			}
+			target = "letter_box_"+target_row+"_"+new_id
+			}else if (new_id == 3 && qwerty.includes(box.textContent) == false){
+			new_id -= 1
+			target = "letter_box_"+target_row+"_"+new_id
+			}
+			box = document.getElementById(target);
+			box.textContent = "";
+			box.style.backgroundColor = "var(--white)";
+			box.style.borderColor = "var(--defaultborder)";
+			box.style.color = "var(--black)";
+		} else if (key == "ENTER" || key == "Enter") {
+			feedback(box);
+		}
+	}
 }
 
+
+function feedback(box) {
+	if (box == box.parentElement.lastChild && box.textContent != ""){ 
+		let guess = ""
+		for (let i = 0; i<box.parentElement.children.length;i++){ 
+			guess += box.parentElement.children[i].textContent
+		}
+		if (airportArray.includes(guess) && guessedCodes.includes(guess) != true){
+			let fluidAnswer = [];
+			guessedCodes.push(guess)
+			for (i=0; i < answer.length; i++) {
+			fluidAnswer.push(answer[i]); 
+			}
+			correctCount = 0;
+			for (let i = 0; i<box.parentElement.children.length;i++) {
+			box.parentElement.children[i].style.transition = "transform " + (i*durationModif+1) + "s ease-in-out, background-color 0.2s " + (i*durationModif+0.8) + "s, border-color 0.2s " + (i*durationModif+0.8) + "s, color 0.2s " + (i*durationModif+0.8) + "s"
+			box.parentElement.children[i].style.transform = "rotate3d(1,0,0,"+ (Math.round((i+3)*360))+"deg)"
+			if (!animatingBox){
+				animatingBox = true
+				window.setTimeout(function(){animatingBox = false},(3000*durationModif+1000))
+			}
+			let guess = box.parentElement.children[i].textContent;
+			let transition = "background-color 0.2s " + (3*durationModif+1.1) + "s, border-color 0.2s "+(3*durationModif+1.1)+"s, color 0.2s "+(3*durationModif+1.1)+"s"
+			if (guess == fluidAnswer[i]) {
+				turnGreen(box.parentElement.children[i], guess, transition);
+				fluidAnswer[i] = "";
+				correctCount += 1;
+			} else if (guess != fluidAnswer[i] && fluidAnswer.includes(guess) == true) {
+				let location = fluidAnswer.indexOf(guess); 
+				if (box.parentElement.children[location].textContent != fluidAnswer[location]){
+					turnYellow(box.parentElement.children[i], guess, transition);
+					fluidAnswer[location] = "";
+				} else {
+					box.parentElement.children[i].style.backgroundColor = "var(--darkgrey)";
+					box.parentElement.children[i].style.borderColor = "var(--darkgrey)";
+					box.parentElement.children[i].style.color = "white";
+				}
+			} else if (fluidAnswer.includes(guess) == false) {
+				turnGrey(box.parentElement.children[i], guess, transition);
+			}
+			}
+			target_row += 1
+			if (target_row == box.parentElement.parentElement.children.length || correctCount == 4) { 
+			finished = true;
+			}
+			if (target_row == 6 && correctCount != 4) {
+			correct = false;
+			} else if (correctCount == 4) {
+			correct = true;
+			window.localStorage.setItem("updateGraph", "true")
+			}
+			if (target_row < box.parentElement.parentElement.children.length && correct == false){
+			window.setTimeout(moveDel, (3000*durationModif+1000))
+			}
+			target = "letter_box_"+target_row+"_"+0
+			if (finished == true) { 
+			updateStats(target_row,correct)
+			timer = window.setTimeout(endScreen, 3000*durationModif+1100)
+			}
+		} else {
+			throwError(guess);
+		}
+	}
+}
+
+function throwError(guess) {
+	if (typeof(document.getElementById("not_a_code_msg")) == 'undefined' || document.getElementById("not_a_code_msg") == null) {
+		let error = document.createElement("div")
+		error.id = "not_a_code_msg"
+		if (guessedCodes.includes(guess)){
+			error.textContent = "Code Already Guessed"
+		}else {
+			if (airportArray == intlCodesArray && allCodesArray.includes(guess)){
+			error.textContent = "Only intl. codes allowed. See settings to change."
+			}else {
+			error.textContent = "Invalid/Missing ICAO Code"
+			}
+		}
+		error.style.top = (document.getElementById("main_container").offsetTop *1.25) + "px";
+		document.body.append(error)
+		error.style.left = (((document.body.clientWidth + 15) / 2) - (error.clientWidth / 2)) + "px"; 
+		error.style.transition = "opacity 1s 1s"
+		error.style.opacity = 0
+		window.setTimeout(function(){
+			error.remove()
+		},2000)
+	}
+}
+
+function turnGreen(box, guess, transition) {
+	box.style.backgroundColor = "var(--green)";
+	box.style.borderColor = "var(--green)";
+	box.style.color = "white";
+	document.getElementById(guess).style.transition = transition
+	document.getElementById(guess).style.backgroundColor = "var(--green)"
+	document.getElementById(guess).style.borderColor = "var(--green)"
+	document.getElementById(guess).style.color = "white"
+}
+
+function turnYellow(box, guess, transition) {
+	box.style.backgroundColor = "var(--yellow)"; 
+	box.style.borderColor = "var(--yellow)";
+	box.style.color = "white";
+	if (document.getElementById(guess).style.backgroundColor != "var(--green)"){
+		document.getElementById(guess).style.transition = transition
+		document.getElementById(guess).style.backgroundColor = "var(--yellow)"
+		document.getElementById(guess).style.borderColor = "var(--yellow)"
+		document.getElementById(guess).style.color = "white"
+	}
+}
+
+function turnGrey(box, guess, transition) {
+	box.style.backgroundColor = "var(--darkgrey)";
+	box.style.borderColor = "var(--darkgrey)";
+	box.style.color = "white";
+	let key = document.getElementById(guess)
+	if (key.style.backgroundColor != "var(--green)" || key.style.backgroundColor == "var(--yellow)") {
+		document.getElementById(guess).style.transition = transition
+		key.style.backgroundColor = "var(--darkgrey)"
+		key.style.borderColor = "var(--darkgrey)"
+		document.getElementById(guess).style.color = "white"
+	}
+}
 
 //When you move to the next row, the delete row button needs to move as well
 
